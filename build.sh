78b8dffbbe29b17d21a30254106942ec5d35da9c
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-08-16 10:33:11 -0700

    Overhaul build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,27 @@ set -e
 
 source ./setup.sh
 
-if [[ "$1" != "--rebuild" ]]
+rebuild=
+debug=
+makeflags=
+
+badopt() {
+	echo "Bad option: $1" >&2
+}
+
+while true
+do
+	case $1 in
+		--rebuild | -r ) rebuild=1 ; shift  ;;
+		--debug | -d ) debug=1 ; shift ;;
+		--clean ) clean=1 ; shift ;;
+		"" ) break ;;
+		-- ) shift ; break ;;
+		* ) badopt $1; exit 1 ;;
+	esac
+done
+
+if [[ $rebuild ]]
 then
 	autoreconf --install
 	cd build
@@ -14,4 +34,14 @@ else
 	cd build
 fi
 
-make
+if [[ $debug ]]
+then
+	makeflags="$makeflags CFLAGS=\"-O0 -DDEBUG\""
+fi
+
+if [[ $clean ]]
+then
+	makeflags="$makeflags clean"
+fi
+
+eval "make $makeflags"
