af7a8af469a427b490a647c0637f8eec27a5ca9d
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-09-20 16:28:05 -0700

    Add comment so I don't forget this stuff later

--- cigar.c
+++ cigar.c
@@ -35,7 +35,7 @@ int run_cigar(  bam1_t *bam,
 	switch(op_type) {
 	case BAM_CDEL:
 		report.align_type = at_del;
-		report.pos = *base_offset - 1;
+		report.pos = *base_offset - 1; // See long comment below for explanation for -1s
 		report.spos = *seq_index - 1;
 		report.data = int_to_b5base(bam_seqi(seq_data, *seq_index));
 		report.size = len;
@@ -44,7 +44,7 @@ int run_cigar(  bam1_t *bam,
 		break;
 	case BAM_CINS:
 		report.align_type = at_ins;
-		report.pos = *base_offset - 1;
+		report.pos = *base_offset - 1; // See long comment below for explanation for -1
 		report.size = len;
 
 		if (*seq_index == 0) {
@@ -72,7 +72,34 @@ int run_cigar(  bam1_t *bam,
 			report.data = int_to_b5base(bam_seqi(seq_data, *seq_index));
 			report.size = 1;
 			if (len == 0 && (next_op_type == BAM_CINS || next_op_type == BAM_CDEL)) {
-				// skip
+				// SKIP! Explanation below. (This is the "large
+				// comment" referenced above.)
+				//
+				// This is the place right before the insertion
+				// or deletion.  By convention, indels are
+				// reported as being at the position prior to
+				// where they actually start.
+				//
+				// For example:
+				//
+				// pos:  200 201 202 203 204 205 206
+				// ref:   A   T   C   C   G
+				// seq:   A  [A   G]  T   C   C   G
+				//            ^ indel
+				// The indel is reported at position 200. The
+				// reasoning behind this is tied to the output
+				// format. Basically, whoever is using the
+				// output doesn't want me to say something like
+				// "At position 200, the sequence matches the
+				// reference and at position 201 there is an
+				// insertion of size 2." It would rather me report this as
+				// "At position 200, the A got turned into AAG."
+				//
+				// Therefore, (a) the match at position 200
+				// should NOT be reported (and that's exactly
+				// what this branch prevents) and (b) the
+				// insertion at position 201 should actually be
+				// reported at 200.
 			} else {
 				rfunc(context, report, extra_data);
 			}
