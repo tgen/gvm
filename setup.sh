d784e35cdbc76b9ddc540ea14e3824924783efa4
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-10-01 18:38:54 -0700

    Migrate setup.sh for dback use
    This won't work on pnap anymore probably!!

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-module load gdb/8.0
 module load gcc/5.1.0 # seems to prevent a very strange bug
 module load samtools
-module load htslib
+module load htslib/git-head
 module load yaml
