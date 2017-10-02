3c0ef4649276b49e1e18173ccfa4194d4d51c758
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-10-02 16:45:29 -0700

    Help setup.sh find modules

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+OLDMODULEPATH=$MODULEPATH
+
+MODULEPATH="$MODULEPATH:/home/skulkarni/privatemodules"
 module load gcc/5.1.0 # seems to prevent a very strange bug
 module load samtools
 module load htslib/git-head
 module load yaml
 module load gengetopt
+
+MODULEPATH=$OLDMODULEPATH
