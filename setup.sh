2998523020dba30997d04c3bd96131a223ec6168
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-10-16 14:35:10 -0700

    If no 'module' command, setup.sh should do nothing

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-OLDMODULEPATH=$MODULEPATH
+if module 2>/dev/null; then
+	OLDMODULEPATH=$MODULEPATH
 
-MODULEPATH="$MODULEPATH:/home/skulkarni/privatemodules"
-module load gcc/5.1.0 # seems to prevent a very strange bug
-module load samtools
-module load htslib/git-head
-module load yaml
-module load gengetopt
+	MODULEPATH="$MODULEPATH:/home/skulkarni/privatemodules"
+	module load gcc/5.1.0 # seems to prevent a very strange bug
+	module load samtools
+	module load htslib/git-head
+	module load yaml
+	module load gengetopt
 
-MODULEPATH=$OLDMODULEPATH
+	MODULEPATH=$OLDMODULEPATH
+
+	echo "Modules loaded."
+fi
