b95a6ab655ab3f8587c9d2a3ad68d526889fca16
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-04-03 14:21:20 -0700

    Add python 3.6.0 as a dependency

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ if module 2>/dev/null; then
 	module load yaml
 	module load gengetopt
 	module load gsl/2.4-sid
+        module load python/3.6.0
 
 	# TEMPORARY STOPGAP MEASURE (HPC ticket 39196)
 	export LIBRARY_PATH="/packages/gsl/2.4/lib:$LIBRARY_PATH"
