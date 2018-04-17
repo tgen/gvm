c8be9c03255b782a9666d3502a387fc31de983bc
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-04-17 15:43:00 -0700

    Add MIN utility macro

--- src/utils.h
+++ src/utils.h
@@ -7,5 +7,6 @@
 #define verbose_fprintf(...) do { if(settings.verbose) fprintf(__VA_ARGS__); } while(0);
 
 #define MAX(X,Y) ( (X) > (Y) ? (X) : (Y) )
+#define MIN(X,Y) ( (X) < (Y) ? (X) : (Y) )
 
 #endif
